f5606e23b5f127df395e0458ba7efffa4782a93e
lalufitho <lalu.fitho8@gmail.com>
2023-05-31 16:43:09 +0800

    a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import image_hero from "../static/images/amico.png";
   import contentImage from "../static/images/bank_sampah.jpeg";
+  import daurUlang from "../static/images/daur_ulang.png";
 </script>
 
 <svelte:head>
@@ -20,6 +21,7 @@
   </div>
   <img class="image_hero" src={image_hero} alt="Hero" />
 </div>
+
 <div class="deskripsi_container">
   <div class="nama">
     <h1>Tracycle</h1>
@@ -36,6 +38,7 @@
     </p>
   </div>
 </div>
+
 <div class="layanan_container">
   <div class="layanan_title">
     <h1>Layanan Yang Kami Sediakan</h1>
@@ -77,5 +80,65 @@
         </p>
       </div>
     </div>
+  </div>
+</div>
+
+<div class="layanan_container">
+  <div class="layanan_title">
+    <h1>Mengapa Kami</h1>
+  </div>
+  <div class="sub_layanan">
+    <div class="layanan">
+      <i class="icon_center fa fa-get-pocket fa-align-center fa-5x" />
+      <div class="sub_layanan_title">
+        <h1>Praktis</h1>
+      </div>
+      <div class="deskripsi_layanan">
+        <p>
+          Pengguna dapat dengan mudah menukar dan mengelola sampah mereka
+          melalui transaksi digital, tanpa repot datang ke tempat pengolahan
+          sampah secara mandiri.
+        </p>
+      </div>
+    </div>
+    <div class="layanan">
+      <i class="icon_center fa fa-truck fa-align-center fa-5x" />
+      <div class="sub_layanan_title">
+        <h1>Efisien</h1>
+      </div>
+      <div class="deskripsi_layanan">
+        <p>
+          Layanan pick-up sampah yang disediakan memungkinkan pengguna untuk
+          mendapatkan sampah mereka dijemput langsung oleh tim Tracycle,
+          menghemat waktu dan tenaga.
+        </p>
+      </div>
+    </div>
+    <div class="layanan">
+      <i class="icon_center fa fa-leaf fa-align-center fa-5x" />
+      <div class="sub_layanan_title">
+        <h1>Lingkungan Bersih</h1>
+      </div>
+      <div class="deskripsi_layanan">
+        <p>
+          Masyarakat dapat lebih mudah dan terorganisir dalam membuang sampah
+          dengan benar, serta mendukung upaya daur ulang dan pengelolaan sampah
+          yang berkelanjutan.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="tukar_container">
+  <img class="image_hero" src={daurUlang} alt="Hero" />
+  <div class="hero_title">
+    <h1>Cara Daur Ulang</h1>
+    <div class="hero_subtitle">
+      Kami menyediakan informasi tentang pengolahan sampah berdasarkan jenisnya,
+      memberikan panduan yang dapat menginspirasi Anda untuk mengubah sampah
+      menjadi produk yang lebih bermanfaat melalui proses daur ulang.
+    </div>
+    <button class="exchange_button">Cara Daur Ulang</button>
   </div>
 </div>
