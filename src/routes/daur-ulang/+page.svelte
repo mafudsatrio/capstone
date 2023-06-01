21a48cc1118cbe7d436c71d1f3c51ef4ce8b9a30
Dena Indiarto Permadi <denaindiartopermadi2002@gmail.com>
2023-06-02 03:12:16 +0800

    daur ulang

---
+++ src/routes/daur-ulang/+page.svelte
@@ -0,0 +1,77 @@
+<script>
+  import Hero from "../../components/Hero.svelte"
+  import image_hero from "/src/static/images/daur_ulang.png";
+  let title = "Tukarkan Sampahmu di Tracycle";
+  let caption = "Mari bergabunglah bersama kami dalam upaya mengelola sampah. Jadikan lingkunganmu nyaman dan bebas dari sampah. Kami siap memberikan dukungan dalam pengelolaan sampah di sekitarmu!";
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<Hero {title} {caption} {image_hero}/>
+
+<div class="layanan_container">
+  <div class="layanan_title">
+    <h1>Cara Daur Ulang Sampah</h1>
+  </div>
+    <div class="deskripsi_layanan">
+          <p>
+            Berikut merupakan cara yang dapat memudahkan anda mendaur ulang sampah
+          </p>
+        </div>
+</div>
+
+<div class=container_daur-ulang>
+
+<div class="half_container">  
+<div class="top">
+    <div class="daur-ulang_icon">
+      <img class="icon" src="/src/static/icons/plastic.png" alt="botol">
+    </div>
+  </div>
+  <div class="bottom">
+    <div class="nama-item_daur-ulang">
+      <p>Plastik</p>
+  </div>
+  </div>
+</div>
+
+<div class="daur-ulang_deskripsi">
+  <div class="desc_title">
+    <h1>Plastik</h1>
+  </div>
+  <div class="desc">
+    <p>Plastik dapat didaur ulang melalui proses seperti pemilahan, pencucian, pelelehan, dan pembentukan menjadi bahan baru. 
+       Dalam daur ulang plastik, bahan plastik bekas diubah menjadi biji plastik atau diolah menjadi produk baru seperti kemasan, 
+       serat tekstil, atau bahan konstruksi.</p>
+  </div>
+</div>
+
+<div class="half_container">  
+  <div class="top">
+      <div class="daur-ulang_icon">
+        <img class="icon" src="/src/static/icons/plastic.png" alt="botol">
+      </div>
+    </div>
+    <div class="bottom">
+      <div class="nama-item_daur-ulang">
+        <p>Plastik</p>
+    </div>
+    </div>
+  </div>
+  
+  <div class="daur-ulang_deskripsi">
+    <div class="desc_title">
+      <h1>Plastik</h1>
+    </div>
+    <div class="desc">
+      <p>Plastik dapat didaur ulang melalui proses seperti pemilahan, pencucian, pelelehan, dan pembentukan menjadi bahan baru. 
+         Dalam daur ulang plastik, bahan plastik bekas diubah menjadi biji plastik atau diolah menjadi produk baru seperti kemasan, 
+         serat tekstil, atau bahan konstruksi.</p>
+    </div>
+  </div>
+  
+
+</div>
