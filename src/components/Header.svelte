807e2c5367d9d98dede79e6e8d8f0480f4d8ae0f
mafudsatrio <riosetiono23@gmail.com>
2023-06-12 19:06:30 +0700

    humberger

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,55 +1,64 @@
 <script>
   import { page } from "$app/stores";
   import logo from "../static/logo/logo.png";
+
+  function Function() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 </script>
 
-<header>
-  <button id="menu" aria-label="button to open side menu" class="hamburger"
-    >☰</button
-  >
-</header>
-<nav id="drawer" class="nav">
-  <a href="/"><img src={logo} alt="Tracycle" class="header__image" /></a>
-  <ul class="nav__list">
-    <li>
-      <a
+  <header class="header">
+    <a href="/"><img src={logo} alt="Tracycle" class="header__image" /></a>
+
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+    <ul class="menu">
+        <li>
+        <a
         class="nav__item {$page.url.pathname === '/' ? 'active' : ''}"
         aria-current={$page.url.pathname === "/" ? "page" : undefined}
         href="/"
-      >
+        >
         Beranda
       </a>
     </li>
     <li>
       <a
-        class="nav__item {$page.url.pathname === '/bank-sampah'
-          ? 'active'
-          : ''}"
-        aria-current={$page.url.pathname === "/bank-sampah"
-          ? "page"
-          : undefined}
-        href="/bank-sampah"
-      >
-        Bank Sampah</a
-      >
-    </li>
-    <li>
-      <a
-        class="nav__item {$page.url.pathname === '/daur-ulang' ? 'active' : ''}"
-        aria-current={$page.url.pathname === "/daur-ulang" ? "page" : undefined}
-        href="/daur-ulang"
-      >
-        Daur Ulang</a
-      >
-    </li>
-    <li>
-      <a
-        class="nav__item {$page.url.pathname === '/tentang' ? 'active' : ''}"
-        aria-current={$page.url.pathname === "/tentang" ? "page" : undefined}
-        href="/tentang"
-      >
-        Tentang</a
-      >
-    </li>
-  </ul>
-</nav>
+          class="nav__item {$page.url.pathname === '/bank-sampah'
+            ? 'active'
+            : ''}"
+          aria-current={$page.url.pathname === "/bank-sampah"
+            ? "page"
+            : undefined}
+          href="/bank-sampah"
+          >
+          Bank Sampah</a
+          >
+        </li>
+        <li>
+          <a
+          class="nav__item {$page.url.pathname === '/daur-ulang' ? 'active' : ''}"
+          aria-current={$page.url.pathname === "/daur-ulang" ? "page" : undefined}
+          href="/daur-ulang"
+          >
+          Daur Ulang</a
+          >
+        </li>
+        <li>
+          <a
+          class="nav__item {$page.url.pathname === '/tentang' ? 'active' : ''}"
+          aria-current={$page.url.pathname === "/tentang" ? "page" : undefined}
+          href="/tentang"
+          >
+          Tentang</a
+          >
+        </li>
+    </ul>
+  </header>
+
+  
