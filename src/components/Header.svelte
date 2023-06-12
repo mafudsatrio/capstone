ee478c94c5aa3e3d7b4e7c69d09878a6c1091677
mafudsatrio <riosetiono23@gmail.com>
2023-06-13 01:13:16 +0700

    formulir pengisian

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -12,51 +12,62 @@
   // }
 </script>
 
-<header class="header">
-  <a href="/"><img src={logo} alt="Tracycle" class="header__image" /></a>
+  <header class="header">
+    <a href="/"><img src={logo} alt="Tracycle" class="header__image" /></a>
 
-  <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
   <label class="menu-icon" for="menu-btn"><span class="navicon" /></label>
-  <ul class="menu">
-    <li>
-      <a
+    <ul class="menu">
+        <li>
+        <a
         class="nav__item {$page.url.pathname === '/' ? 'active' : ''}"
         aria-current={$page.url.pathname === "/" ? "page" : undefined}
         href="/"
-      >
+        >
         Beranda
       </a>
     </li>
     <li>
       <a
-        class="nav__item {$page.url.pathname === '/bank-sampah'
-          ? 'active'
-          : ''}"
-        aria-current={$page.url.pathname === "/bank-sampah"
-          ? "page"
-          : undefined}
-        href="/bank-sampah"
-      >
-        Bank Sampah</a
-      >
-    </li>
-    <li>
-      <a
-        class="nav__item {$page.url.pathname === '/daur-ulang' ? 'active' : ''}"
-        aria-current={$page.url.pathname === "/daur-ulang" ? "page" : undefined}
-        href="/daur-ulang"
-      >
-        Daur Ulang</a
-      >
-    </li>
-    <li>
-      <a
-        class="nav__item {$page.url.pathname === '/tentang' ? 'active' : ''}"
-        aria-current={$page.url.pathname === "/tentang" ? "page" : undefined}
-        href="/tentang"
-      >
-        Tentang</a
-      >
-    </li>
-  </ul>
-</header>
+          class="nav__item {$page.url.pathname === '/bank-sampah'
+            ? 'active'
+            : ''}"
+          aria-current={$page.url.pathname === "/bank-sampah"
+            ? "page"
+            : undefined}
+          href="/bank-sampah"
+          >
+          Bank Sampah</a
+          >
+        </li>
+        <li>
+          <a
+          class="nav__item {$page.url.pathname === '/daur-ulang' ? 'active' : ''}"
+          aria-current={$page.url.pathname === "/daur-ulang" ? "page" : undefined}
+          href="/daur-ulang"
+          >
+          Daur Ulang</a
+          >
+        </li>
+        <li>
+          <a
+          class="nav__item {$page.url.pathname === '/tentang' ? 'active' : ''}"
+          aria-current={$page.url.pathname === "/tentang" ? "page" : undefined}
+          href="/tentang"
+          >
+          Tentang</a
+          >
+        </li>
+        <li>
+          <a
+          class="nav__item {$page.url.pathname === '/formulir-pengisian' ? 'active' : ''}"
+          aria-current={$page.url.pathname === "/formulir-pengisian" ? "page" : undefined}
+          href="/formulir-pengisian"
+          >
+          Formulir Pengisian</a
+          >
+        </li>
+    </ul>
+  </header>
+
+  
---
+++ src/routes/formulir-pengisian/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Head from "../../components/Head.svelte";
+  import image_hero from "/src/static/images/image_page.png";
+</script>
+
+<svelte:head>
+  <title>Tracycle - Tentang</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<Head
+  title="Kami Siap Mendengar Anda"
+  caption="Hubungi Kami, Konsultasikan Ide Anda, dan Berikan Feedback yang Berharga. "
+  {image_hero}
+/>
+
+<div class="layanan_container">
+  <div class="layanan_title">
+    <h1>Formulir Pengisian Biodata Tracycle</h1>
+    <hr />
+  </div>
+</div>
