917e374053e77bcb883b3ce5b972b0be79349b4d
Dena Indiarto Permadi <denaindiartopermadi2002@gmail.com>
2023-06-14 00:43:14 +0800

    update icon bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
   import Header from "../components/Header.svelte";
   import Footer from "../components/Footer.svelte";
-  import logo_white from "../static/logo/logo_white.png";
+  import logo_white from "../static/favicon.png";
   import "/src/styles/main.css";
   import "/src/styles/responsive.css";
 </script>
